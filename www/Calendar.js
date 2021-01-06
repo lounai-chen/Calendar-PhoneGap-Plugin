ccc6e6bbc732e423d1820bc7337c1c43c27f35c2
May.lou <2419538202@qq.com>
2021-01-06 15:16:41 +0800

    Update Calendar.js

--- www/Calendar.js
+++ www/Calendar.js
@@ -16,6 +16,31 @@ var Calendar = {
   listCalendars : function (successCallback, errorCallback) {
      exec(successCallback, errorCallback, "Calendar", "listCalendars", []);
   },
+ createEventWithOptions : function (title, location, notes, startDate, endDate, options, successCallback, errorCallback) {
+    if (!(startDate instanceof Date && endDate instanceof Date)) {
+      errorCallback("startDate and endDate must be JavaScript Date Objects");
+      return;
+    }
+
+    // merge passed options with defaults
+    var mergedOptions = Calendar.prototype.getCalendarOptions();
+    for (var val in options) {
+      if (options.hasOwnProperty(val)) {
+        mergedOptions[val] = options[val];
+      }
+    }
+    if (options.recurrenceEndDate != null) {
+      mergedOptions.recurrenceEndTime = options.recurrenceEndDate.getTime();
+    }
+    exec(successCallback, errorCallback, "Calendar", "createEventWithOptions", [{
+      "title": title,
+      "location": location,
+      "notes": notes,
+      "startTime": startDate instanceof Date ? startDate.getTime() : null,
+      "endTime": endDate instanceof Date ? endDate.getTime() : null,
+      "options": mergedOptions
+    }]);
+  },
 }
 
 module.exports = Calendar;
